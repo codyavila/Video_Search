83004d3a8d163a751705411ac483772da21df74a
sanderscody26 <sanderscody26@gmail.com>
2022-09-03 15:05:35 -0400

    Video now passes to VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
 
-const VideoCard = () => {
-  return (
-    <div>
-      video
-    </div>
-  )
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet
+  }
+}) => {
+  console.log(videoId, snippet)
+  return <div>video</div>
 }
 
 export default VideoCard
