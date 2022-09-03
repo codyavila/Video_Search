f4f454a82a43498ea1defda66ac125d4a923571c
sanderscody26 <sanderscody26@gmail.com>
2022-09-03 15:30:08 -0400

    Channel Card now displays Data to user

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,8 +8,30 @@ const VideoCard = ({
     snippet
   }
 }) => {
-  console.log(videoId, snippet)
-  return <div>video</div>
+  return (
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+      <Link to={`/video/${videoId}`}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={`/video/${videoId}`}>
+          <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
+            {snippet?.title.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={`/channel/${snippet?.channelId}`}>
+          <Typography variant='subtitle1' fontWeight='bold' color='gray'>
+            {snippet?.channelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default VideoCard
