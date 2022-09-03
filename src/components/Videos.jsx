6d68d0c69827a2b7ae8927829ec8e7fb7447c595
sanderscody26 <sanderscody26@gmail.com>
2022-09-03 16:26:13 -0400

    Search bar now makes API call and is functional

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,7 @@ import { VideoCard, ChannelCard } from './'
 const Videos = ({ videos }) => {
   return (
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-      {videos.map((item, index) => (
+      {videos?.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
