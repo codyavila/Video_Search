857be01eb86253ee308fd51e10ab784ea6d2d101
sanderscody26 <sanderscody26@gmail.com>
2022-09-03 13:54:46 -0400

    Created Heading for Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,6 +20,16 @@ const Feed = () => {
           Copyright 2022 Cody Avila
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}>
+          New <span style={{ color: '#f31503' }}>Videos</span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
